f092aa19ddf19ff04530e7b6d4cda188ec334d45
NyxVoid <amrit44404@proton.me>
2024-12-28 10:27:30 +0545

    removed htop

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ doas pkg_add -uvi && doas pkg_add -vi harfbuzz \
 	unzip zathura zathura-pdf-poppler scrot \
 	rust go ripgrep hugo wget transmission \
 	ubuntu-nerd-fonts screenfetch intel-media-driver
-# xdg-user-dirs xdg-utils
+# xdg-user-dirs xdg-utils htop
 # gimp obs xf86-video-intel
 # wget nodejs
 # font-hack-ttf
