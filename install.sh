258d00f646dd689b1c44b01a36b1eaf17d55b148
amritxyz <amrit44404@proton.me>
2025-01-17 21:54:04 +0545

    deluge

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ doas pkg_add -uvi && doas pkg_add -vi harfbuzz \
 	ffmpeg xwallpaper xclip xdotool lf adwaita-icon-theme \
 	firefox nsxiv neovim mpv newsboat cmixer neofetch \
 	unzip zathura zathura-pdf-poppler scrot \
-	rust go ripgrep hugo wget \
+	rust go ripgrep hugo wget deluge \
 	intel-media-driver terminus-nerd-fonts
 # xdg-user-dirs xdg-utils htop
 # gimp obs xf86-video-intel
